fea38787f2bc278d40d3e7bc6bca21e760d68a1d
sairakhan07 <sairaislamkhan@gmail.com>
2026-03-24 13:16:47 +0000

    Add song management methods to Playlist class
    Implemented methods to manage songs in the playlist.

--- Playlist.java
+++ Playlist.java
@@ -1,3 +1,65 @@
+import java.util.ArrayList;
+
+public class Playlist
+{
+    ArrayList<Song>songs;
+    
+    addSong(Song song)
+    {
+    }
+
+    removeSong(Song song)
+    {
+    }
+
+    displayPlaylist()
+    {
+    }
+
+    displaySongsAfterXPlays(int playCount)
+    {
+    }
+}
+
+class Song
+{
+    String songTitle;
+    String artistName;
+    int playCount;
+
+    public String getSongTitle(String songTitle) 
+    {
+        return songTitle;
+    }
+
+    public String getArtistName(String artistName) 
+    {
+        return artistName;
+    }
+
+    public int getPlayCount(int playCount)
+    {
+        return playCount;
+    }
+
+    public void setSongTitle(String songTitle) 
+    {
+        this.songTitle = songTitle;
+    }
+
+    public void setArtistName(String artistName) 
+    {
+        this.artistName = artistName;
+    }
+
+    public void setPlayCount(int playCount) 
+    {
+        this.playCount = playCount; 
+    }
+
+
+}
+
 public class Playlist
 {
 
