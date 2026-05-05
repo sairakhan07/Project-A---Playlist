f28cc935ce8ab74c3c70f10789b33a82a5a92503
sairakhan07 <sairaislamkhan@gmail.com>
2026-05-05 09:43:37 +0100

    Refactor Main class to include specific songs
    Updated the Main class to add specific songs with play counts and modified the removal of a song from the playlist.

--- Playlist.java
+++ Playlist.java
@@ -86,34 +86,33 @@ public class Main
     {
         Playlist myPlaylist = new Playlist();
         
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
-        myPlaylist.addSong(new Song());
+        myPlaylist.addSong(new Song("Shape of You", "Ed Sheeran", 4893718356));
+        myPlaylist.addSong(new Song("D O D O", "Tayc", 172800535));
+        myPlaylist.addSong(new Song("Havana", "Camilla Cabello", 2465034444));
+        myPlaylist.addSong(new Song("No Scrubs", "TLC", 1575003362));
+        myPlaylist.addSong(new Song("Fill Me In", "Craig David", 70010778));
+        myPlaylist.addSong(new Song("Just the Way You Are", " Bruno Mars", 3255275101));
+        myPlaylist.addSong(new Song("Genie in a Bottle", "Christina Aguilera", 740794605));
+        myPlaylist.addSong(new Song("Maria Maria", "Santana", 626285217));
+        myPlaylist.addSong(new Song("Flatline", "Justin Bieber", 286411374));
+        myPlaylist.addSong(new Song("Crazy In Love", "Beyonce", 1834515045));
+        myPlaylist.addSong(new Song("Smooth Criminal", "Michael Jackson", 1159042));
+        myPlaylist.addSong(new Song("Beautiful Girls", "Shaun Kingston", 1118596046));
 
         System.out.println("---My playlist---");
         myPlaylist.displayPlaylist();
         System.out.println();
         System.out.println();
 
-        System.out.println("Removing '' from playlist...");
-        myPlaylist.removeSong("");
+        System.out.println("Removing 'Shape of You' from playlist...");
+        myPlaylist.removeSong("Shape of You");
         myPlaylist.displayPlaylist();
         System.out.println();
         System.out.println();
 
-        int minPlays = 100000;
+        int minPlays = 1000000000;
         System.out.println("Songs with more than " + minPlays + " plays:");
         myPlaylist.displaySongsAfterXPlays(minPlays);
 
     }
 }
-
